354138686b8c9e4e166417aa9152371d12c0da85
Fernando Alfonso Barrera Ortiz <ferbarotz23@gmail.com>
2026-09-01 17:28:01 +0000

    Reestructurar: mover Home.jsx a src/front/page/Home y actualizar imports de Home y Navbar

--- src/front/main.jsx
+++ src/front/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./Home/Home.jsx";
+import Home from "./page/Home/Home";
 
 // Punto de entrada de la aplicación React.
 // Monta el componente principal <Home /> dentro del div#root de index.html.
--- src/front/page/Home/Home.jsx
+++ src/front/page/Home/Home.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../Componente/Navbar.jsx";
+import Navbar from "../../Componente/Navbar";
 
 const Home = () => {
   const styles = {
